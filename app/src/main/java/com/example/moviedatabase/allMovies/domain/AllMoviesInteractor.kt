d83baeb3236bf4c468be2dc47b9ff2efc65c71b7
ali <mali.guitagechi@gmail.com>
2024-08-05 10:43:08 +0200

    refactor(architecture): add AllMoviesInteractor

---
+++ app/src/main/java/com/example/moviedatabase/allMovies/domain/AllMoviesInteractor.kt
@@ -0,0 +1,15 @@
+package com.example.moviedatabase.allMovies.domain
+
+import com.example.moviedatabase.allMovies.domain.entity.Movie
+import com.example.moviedatabase.allMovies.domain.repository.AllMoviesRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class AllMoviesInteractor @Inject constructor(
+    private val allMoviesRepository: AllMoviesRepository
+) {
+    operator fun invoke(): Flow<List<Movie>> = allMoviesRepository.getAllMovies()
+        .flowOn(Dispatchers.IO)
+}
