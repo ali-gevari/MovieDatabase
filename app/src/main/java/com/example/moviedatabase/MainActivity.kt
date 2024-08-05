2b9c16a0b064229618597ee62b2c6e29600adb2f
ali <mali.guitagechi@gmail.com>
2024-08-05 09:47:41 +0200

    feat(allMovies): finalize the MainActivity and integrate the AllMoviesScreen

--- app/src/main/java/com/example/moviedatabase/MainActivity.kt
+++ app/src/main/java/com/example/moviedatabase/MainActivity.kt
@@ -1,47 +1,49 @@
 package com.example.moviedatabase
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.repeatOnLifecycle
+import com.example.moviedatabase.allMovies.presentation.AllMoviesScreen
+import com.example.moviedatabase.globalEvents.Event
+import com.example.moviedatabase.globalEvents.EventDispatcher
 import com.example.moviedatabase.ui.theme.MovieDatabaseTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MovieDatabaseTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val lifecycle = LocalLifecycleOwner.current.lifecycle
+                LaunchedEffect(key1 = lifecycle) {
+                    repeatOnLifecycle(state = Lifecycle.State.STARTED) {
+                        EventDispatcher.events.collect { event ->
+                            if (event is Event.Toast) {
+                                Toast.makeText(this@MainActivity, event.message, Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        }
+                    }
+                }
+                Surface(
+                    modifier = Modifier.fillMaxWidth(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AllMoviesScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MovieDatabaseTheme {
-        Greeting("Android")
     }
 }
