045c72a0bb6083e494b57396bb0eac6db2e48d9e
ali <mali.guitagechi@gmail.com>
2024-08-05 08:10:36 +0200

    feat(allMovies): add the data layer api call and repository implementation

---
+++ app/src/main/java/com/example/moviedatabase/allMovies/data/mapper/Mapper.kt
@@ -0,0 +1,18 @@
+package com.example.moviedatabase.allMovies.data.mapper
+
+import coil.network.HttpException
+import com.example.moviedatabase.allMovies.domain.entity.NetworkCallError
+import com.example.moviedatabase.allMovies.domain.entity.NetworkError
+import java.io.IOException
+
+fun Throwable.toNetworkError(): NetworkError {
+    val error = when(this) {
+        is IOException -> NetworkCallError.NetworkError
+        is HttpException -> NetworkCallError.UnknownResponse
+        else -> NetworkCallError.UnknownError
+    }
+    return NetworkError(
+        networkCallError = error,
+        this
+    )
+}
