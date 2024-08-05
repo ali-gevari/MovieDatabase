801dff8b8284fd630e8f79b68bf94a93517347d1
ali <mali.guitagechi@gmail.com>
2024-08-05 10:46:05 +0200

    refactor(architecture): update viewModel to react to interactor instead of repository

--- app/src/main/java/com/example/moviedatabase/allMovies/presentation/AllMoviesViewModel.kt
+++ app/src/main/java/com/example/moviedatabase/allMovies/presentation/AllMoviesViewModel.kt
@@ -2,19 +2,24 @@ package com.example.moviedatabase.allMovies.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.moviedatabase.allMovies.data.mapper.toNetworkError
+import com.example.moviedatabase.allMovies.domain.AllMoviesInteractor
 import com.example.moviedatabase.allMovies.domain.repository.AllMoviesRepository
 import com.example.moviedatabase.globalEvents.Event
 import com.example.moviedatabase.util.extensions.sendEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class AllMoviesViewModel @Inject constructor(
-    private val allMoviesRepository: AllMoviesRepository
+    private val allMoviesInteractor: AllMoviesInteractor
 ) : ViewModel() {
     private val _state = MutableStateFlow(AllMoviesViewState())
     val state = _state.asStateFlow()
@@ -25,24 +30,29 @@ class AllMoviesViewModel @Inject constructor(
 
     private fun getAllMovies() {
         viewModelScope.launch {
-            _state.update {
-                it.copy(isLoading = true)
-            }
-            allMoviesRepository.getAllMovies()
-                .onRight { allMovies ->
+            allMoviesInteractor()
+                .onStart {
+                    _state.update {
+                        it.copy(isLoading = true)
+                    }
+                }
+                .catch { error ->
+                    val catch = error.toNetworkError().networkCallError.message
+                    _state.update {
+                        it.copy(error = catch)
+                    }
+                    sendEvent(Event.Toast(catch))
+                }
+                .onCompletion {
+                    _state.update {
+                        it.copy(isLoading = false)
+                    }
+                }
+                .collect { allMovies ->
                     _state.update {
                         it.copy(allMovies = allMovies)
                     }
                 }
-                .onLeft { error ->
-                    _state.update {
-                        it.copy(error = error.networkCallError.message)
-                    }
-                    sendEvent(Event.Toast(error.networkCallError.message))
-                }
-            _state.update {
-                it.copy(isLoading = false)
-            }
         }
     }
 }
