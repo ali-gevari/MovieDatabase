e6417627a0d16f680f9c85391f0a5fb0c281c53b
ali <mali.guitagechi@gmail.com>
2024-08-05 10:44:45 +0200

    refactor(architecture): update repository and remove Either and suspend functions

--- app/src/main/java/com/example/moviedatabase/allMovies/domain/repository/AllMoviesRepository.kt
+++ app/src/main/java/com/example/moviedatabase/allMovies/domain/repository/AllMoviesRepository.kt
@@ -1,9 +1,8 @@
 package com.example.moviedatabase.allMovies.domain.repository
 
-import arrow.core.Either
 import com.example.moviedatabase.allMovies.domain.entity.Movie
-import com.example.moviedatabase.allMovies.domain.entity.NetworkError
+import kotlinx.coroutines.flow.Flow
 
 interface AllMoviesRepository {
-    suspend fun getAllMovies(): Either<NetworkError, List<Movie>>
+    fun getAllMovies(): Flow<List<Movie>>
 }
