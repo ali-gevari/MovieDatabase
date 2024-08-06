d3b8bee22cd0e5d84997ed32f95dc3d1145f4aad
ali <mali.guitagechi@gmail.com>
2024-08-06 08:10:49 +0200

    feat(global): create AllShowsScreen and modify the AllMoviesScreen

--- app/src/main/java/com/example/moviedatabase/allMovies/presentation/AllMoviesScreen.kt
+++ app/src/main/java/com/example/moviedatabase/allMovies/presentation/AllMoviesScreen.kt
@@ -1,21 +1,21 @@
 package com.example.moviedatabase.allMovies.presentation
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
-import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
-import androidx.compose.foundation.lazy.staggeredgrid.items
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.example.moviedatabase.compose.Loading
 import com.example.moviedatabase.compose.MovieCard
-import com.example.moviedatabase.compose.MoviesTopAppBar
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
 
 @Composable
 internal fun AllMoviesScreen(
@@ -29,19 +29,23 @@ internal fun AllMoviesScreen(
 fun AllMoviesContent(
     state: AllMoviesViewState
 ) {
-    Loading(isLoading = state.isLoading)
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        topBar = { MoviesTopAppBar(title = "Movies") }
+    Column(
+        modifier = Modifier.padding(8.dp)
     ) {
-        LazyVerticalStaggeredGrid(
-            modifier = Modifier.padding(top = it.calculateTopPadding()),
-            columns = StaggeredGridCells.Fixed(2),
-            horizontalArrangement = Arrangement.spacedBy(10.dp),
-            verticalItemSpacing = 10.dp
+        Text(
+            text = "Movies",
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+        LazyRow(
+            modifier = Modifier
+                .padding(vertical = 8.dp)
+                .height(360.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            contentPadding = PaddingValues(horizontal = 16.dp)
         ) {
             items(state.allMovies) { movie ->
-                MovieCard(movie = movie)
+                MovieCard(title = movie.title, image = movie.posterPath)
             }
         }
     }
